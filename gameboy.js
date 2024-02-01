cd00106a991a88ec5790c369ab2adb77df0b7c6b
Zi Page <59000769+zipage@users.noreply.github.com>
2024-02-01 03:04:13 -0700

    linear animation
    This has the linear animation for the "start" page.

---
+++ gameboy.js
@@ -0,0 +1,7 @@
+$("#btnA").click(function () {
+    $('.buttons').toggleClass('animateMe');
+});
+
+
+//https://forum.freecodecamp.org/t/how-to-start-a-function-only-on-a-button-click/476327
+//https://stackoverflow.com/questions/41587802/css-animation-from-left-to-right
